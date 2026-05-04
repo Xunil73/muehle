d7fe243a6ae1b48fd6ad26a163dc2c2e41443204
Harry <dj5my@ok.de>
2026-05-04 09:00:46 +0200

    regex seems to work - hh, hhmm, hhmmss, hh:mm, hh:mm:ss is parsed to: hh:mm:ss - return a string

--- zeitRegex.py
+++ zeitRegex.py
@@ -3,14 +3,17 @@
 import sys
 import re
 
-def createTimestring():
-  arg=sys.argv[1]
+# Funktion erzeugt aus einer Zeiteingabe (hh, hhmm, hhmmss, hh:mm, hh:mm:ss) einen vollständigen
+# String mit der Uhrzeit -> hh:mm:ss
+def createTimestring(tme):
+  arg=tme
 
   pattern=[ r'^[0-9][0-9]$',\
             r'^[0-9][0-9][0-9][0-9]$',\
             r'^[0-9][0-9][0-9][0-9][0-9][0-9]$',\
             r'^[0-9][0-9]:[0-9][0-9]$',\
-            r'^[0-9][0-9]:[0-9][0-9]:[0-9][0-9]$' ]
+            r'^[0-9][0-9]:[0-9][0-9]:[0-9][0-9]$',\
+            r'.*' ]
 
   for i, ele in enumerate(pattern):
     if re.match(ele, arg):
@@ -18,10 +21,15 @@ def createTimestring():
         arg=arg+':00:00'
       if i == 1:
         arg=arg[:2]+':'+arg[2:]+':00'
-      if i == 3:
+      if i == 2:
         arg=arg[:2]+':'+arg[2:4]+':'+arg[4:]
-# here continue.... (and test the above)
+      if i == 3:
+        arg=arg+':00'
+      if i == 4:
+        break
+      if i == 5:
+        print('pymtm: kann Argument nicht verarbeiten')
+        sys.exit(-1)
     re.match(ele, arg) and print("auch das geht...")
 
-  print(arg)
-createTimestring()
+  return arg
