0a7206e4aab41fdc93f290b760b68b215d9746e9
Harry <dj5my@ok.de>
2026-05-07 20:25:10 +0200

    now we have timecompensation from database at default when computing a new measure point. And now we can switch it off by calling the program with the flag -n or --no-compensation

--- timediff.py
+++ timediff.py
@@ -6,6 +6,18 @@ import subprocess
 from mtm_pyutils.zeitRegex import createTimestring
 from mtm_pyutils.gt10sek import gt10sekInFuture
 import sqlite3
+import argparse
+
+# damit wandeln wir einen String mit Uhrzeit in einen Zeitstempel im UNIX Format.
+def strtotmestmp(timestring): # string to timestamp -> takes a HH:MM:SS string and
+                              # converts it to an unix-timestamp
+  return datetime.combine(datetime.now().date(), \
+         datetime.strptime(timestring, '%H:%M:%S').time()).timestamp()
+
+
+parse=argparse.ArgumentParser()
+parse.add_argument('-n', '--no-compensation', dest='nocompensation', action="store_true")
+args=parse.parse_args()
 
 # wir lesen den Inhalt der mtm.conf und wissen wo die Datenbank ist und wie 
 # sie heisst.
@@ -15,38 +27,43 @@ conffile.close()                 # Zeilenumbruchzeichens in der .conf die Datenb
 
 # wir holen uns den letzten Messwert und nehmen ihn als Richtlinie für die 
 # Kompensation der Zeit bei der gestoppt werden soll.
+# Vorher habe ich die Systemzeit des Computers als Startpunkt genommen, dann bis zu den nächsten
+# fünf vollen Sekunden aufgefüllt und nochmal zehn addiert. Die Praxis hat gezeigt dass, im Falle die 
+# Testuhr geht 30 Sekunden nach, dies einen zu langen Zeitraum dauert bis man an den Messpunkt kommt.
+# Umgekehrt gilt, die Zeit zum Messpunkt wird unpraktikabel kurz wenn die Uhr deutlich vor geht.
+# Deshalb habe ich den Startpunkt nochmals um den Betrag, der zuletzt bei der Uhr gemessen wurde, verschoben.
+# Nun haben wir eine annähernde Angleichung von berechnetem Meßpunkt und dem momentanen Vor/Nachgang der Testuhr 
 verbindung=sqlite3.connect(conftext)
 zeiger=verbindung.cursor()
 zeiger.execute("SELECT timedelta FROM muehle ORDER BY date DESC, time DESC LIMIT 1;")
 inhalt=zeiger.fetchall()
 verbindung.close()
 inhalt=inhalt[0][0] # fetchall gibt ein Tupel (nur index 0 belegt?!) in einer Liste zurück e.g. [(wert, )]
+if args.nocompensation:
+  inhalt=0
 
-def strtotmestmp(timestring): # string to timestamp -> takes a HH:MM:SS string and
-                              # converts it to an unix-timestamp
-  return datetime.combine(datetime.now().date(), \
-         datetime.strptime(timestring, '%H:%M:%S').time()).timestamp()
-
+# die Funktion ohne Integerargument berechnet eine Zeit in der Zukunft, auf volle 5 Sek. teilbar
+# und diese Zeit liegt 10 bis 15 Sekunden vor der Systemzeit des Computers
+# die Funktion mit Integerargument berechnet eine Zeit in der Zukunft, auf volle 5 Sek. teilbar
+# und diese Zeit liegt ungefähr 10 bis 15 Sekunden vor der Zeit der Testuhr. Als Argument dient in 
+# diesem Fall der Messwert des zuletzt gemessenen Vor/Nachgangs als Integer.
 sollZeit=gt10sekInFuture(inhalt)
 
 # wir rechnen hier mit der Unix-Zeit. Zeit des Programmstarts minus
 # den gemessenen Wert (.timestamp = UNIX TIME)
-unixtime_eingabe=strtotmestmp(sollZeit)
+unixtime_eingabe=strtotmestmp(sollZeit) # Das ist die Zeit der Testuhr bei
+# der wir eine Messung auslösen
 
 satisfied=False
-
 while not satisfied:
 
   keyPressed=input('Drücken Sie die Eingabetaste exakt um %s Uhr ...' % sollZeit)
-
+  # es wurde gedrueckt und wir halten die aktuelle Zeit nach PTB / Systemzeit fest
   unixtime_now=datetime.now().timestamp()
-
   diff=unixtime_eingabe-unixtime_now
 
   saveit=['y', 'Y', 'Yes', 'YES', 'yes', 'j', 'J', 'Ja', 'ja']
-
   repeatit=['a', 'A', 'again', 'Again', 'n', 'N', 'nein', 'Nein', 'no', 'No', 'NEIN', 'NO']
-
   finishit=['q', 'Q', 'Quit', 'quit', 'QUIT']
 
   print('Abweichung zur Referenzzeit: %.1f Sekunden.' % diff)
