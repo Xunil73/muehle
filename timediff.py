8fdd96b9b790cb1d9895555c9e9e95c621ce6581
Harry <dj5my@ok.de>
2026-05-07 23:12:58 +0200

    cleaned up code, renamed variables

--- timediff.py
+++ timediff.py
@@ -9,14 +9,16 @@ import sqlite3
 import argparse
 
 # damit wandeln wir einen String mit Uhrzeit in einen Zeitstempel im UNIX Format.
-def strtotmestmp(timestring): # string to timestamp -> takes a HH:MM:SS string and
-                              # converts it to an unix-timestamp
+def strToTimestamp(timestring):
   return datetime.combine(datetime.now().date(), \
          datetime.strptime(timestring, '%H:%M:%S').time()).timestamp()
 
 
 parse=argparse.ArgumentParser()
-parse.add_argument('-n', '--no-compensation', dest='nocompensation', action="store_true", help="next measure point is computed from system's clock, not testwatch")
+parse.add_argument('-n', '--no-compensation',\
+                   dest='nocompensation',\
+                   action="store_true",\
+                   help="next measure point is computed from system's clock, not testwatch")
 args=parse.parse_args()
 
 # wir lesen den Inhalt der mtm.conf und wissen wo die Datenbank ist und wie 
@@ -33,34 +35,34 @@ conffile.close()                 # Zeilenumbruchzeichens in der .conf die Datenb
 # Umgekehrt gilt, die Zeit zum Messpunkt wird unpraktikabel kurz wenn die Uhr deutlich vor geht.
 # Deshalb habe ich den Startpunkt nochmals um den Betrag, der zuletzt bei der Uhr gemessen wurde, verschoben.
 # Nun haben wir eine annähernde Angleichung von berechnetem Meßpunkt und dem momentanen Vor/Nachgang der Testuhr 
-verbindung=sqlite3.connect(conftext)
-zeiger=verbindung.cursor()
-zeiger.execute("SELECT timedelta FROM muehle ORDER BY date DESC, time DESC LIMIT 1;")
-inhalt=zeiger.fetchall()
-verbindung.close()
-inhalt=inhalt[0][0] # fetchall gibt ein Tupel (nur index 0 belegt?!) in einer Liste zurück e.g. [(wert, )]
+connection=sqlite3.connect(conftext)
+pntr=connection.cursor()
+pntr.execute("SELECT timedelta FROM muehle ORDER BY date DESC, time DESC LIMIT 1;")
+compensationTime=pntr.fetchall()
+connection.close()
+compensationTime=compensationTime[0][0] # fetchall gibt ein Tupel (nur index 0 belegt?!) in einer Liste zurück e.g. [(wert, )]
 if args.nocompensation:
-  inhalt=0
+  compensationTime=0
 
 # die Funktion ohne Integerargument berechnet eine Zeit in der Zukunft, auf volle 5 Sek. teilbar
 # und diese Zeit liegt 10 bis 15 Sekunden vor der Systemzeit des Computers
 # die Funktion mit Integerargument berechnet eine Zeit in der Zukunft, auf volle 5 Sek. teilbar
 # und diese Zeit liegt ungefähr 10 bis 15 Sekunden vor der Zeit der Testuhr. Als Argument dient in 
 # diesem Fall der Messwert des zuletzt gemessenen Vor/Nachgangs als Integer.
-sollZeit=gt10sekInFuture(inhalt)
+newTimeMeasurePoint=gt10sekInFuture(compensationTime)
 
 # wir rechnen hier mit der Unix-Zeit. Zeit des Programmstarts minus
 # den gemessenen Wert (.timestamp = UNIX TIME)
-unixtime_eingabe=strtotmestmp(sollZeit) # Das ist die Zeit der Testuhr bei
+unixtime_time_goal=strToTimestamp(newTimeMeasurePoint) # Das ist die Zeit der Testuhr bei
 # der wir eine Messung auslösen
 
 satisfied=False
 while not satisfied:
 
-  keyPressed=input('Drücken Sie die Eingabetaste exakt um %s Uhr ...' % sollZeit)
+  keyPressed=input('Drücken Sie die Eingabetaste exakt um %s Uhr ...' % newTimeMeasurePoint)
   # es wurde gedrueckt und wir halten die aktuelle Zeit nach PTB / Systemzeit fest
   unixtime_now=datetime.now().timestamp()
-  diff=unixtime_eingabe-unixtime_now
+  diff=unixtime_time_goal-unixtime_now
 
   saveit=['y', 'Y', 'Yes', 'YES', 'yes', 'j', 'J', 'Ja', 'ja']
   repeatit=['a', 'A', 'again', 'Again', 'n', 'N', 'nein', 'Nein', 'no', 'No', 'NEIN', 'NO']
@@ -73,9 +75,9 @@ while not satisfied:
 
   if question in repeatit:
     print()
-    neuerEingabestring=gt10sekInFuture(inhalt)
-    sollZeit=neuerEingabestring
-    unixtime_eingabe=strtotmestmp(neuerEingabestring)
+    newTimeGoal=gt10sekInFuture(compensationTime)
+    newTimeMeasurePoint=newTimeGoal
+    unixtime_time_goal=strToTimestamp(newTimeGoal)
     
   if question in finishit:
     sys.exit(0)
