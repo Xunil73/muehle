46c83e5830fc112569f85187b95e90be02352cc0
Harry <dj5my@ok.de>
2026-05-13 11:03:44 +0200

    set back to basic funcs. now experimenting with layout

--- mtmgraph.py
+++ mtmgraph.py
@@ -2,6 +2,7 @@
 
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import numpy as np
 from datetime import datetime
 import subprocess
 
@@ -21,30 +22,6 @@ for ele in output.splitlines():
 x = [datetime.strptime(d, "%Y-%m-%d %H:%M:%S") for d, y in daten]
 y = [y for d, y in daten]
 
-# Figure erzeugen
-fig, ax = plt.subplots(figsize=(8, 4))
+plt.plot(x, y)
 
-# Linie zeichnen
-ax.plot(x, y, marker="o")
-
-# Y-Achse von -50 bis +50
-ax.set_ylim(-50, 50)
-
-# Gitter einschalten
-ax.grid(True)
-
-# X-Achse: 1 Hauptgitter pro Tag
-ax.xaxis.set_major_locator(mdates.DayLocator())
-
-# Datumsformat
-ax.xaxis.set_major_formatter(mdates.DateFormatter("%d.%m"))
-
-# Beschriftung drehen
-plt.xticks(rotation=45)
-
-# Layout optimieren
-plt.tight_layout()
-
-# Anzeigen
 plt.show()
-
