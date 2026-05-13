61ae3793f6cdeb18449fda4265d2381a9d84972b
Harry <dj5my@ok.de>
2026-05-13 11:25:13 +0200

    experimenting again...

--- mtmgraph.py
+++ mtmgraph.py
@@ -22,6 +22,30 @@ for ele in output.splitlines():
 x = [datetime.strptime(d, "%Y-%m-%d %H:%M:%S") for d, y in daten]
 y = [y for d, y in daten]
 
+# Anzahl Tage bestimmen
+anzahl_tage = (max(x) - min(x)).days + 1
+
+# 2 cm pro Tag
+breite_cm = anzahl_tage * 2
+
+# cm → inch
+breite_inch = breite_cm / 2.54
+
+# Figure erzeugen
+fig, ax = plt.subplots(figsize=(breite_inch, 5))
+
+# Plot
+ax.plot(x, y)
+
+# Datumsformat
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%d.%m'))
+
+# automatische Datums-Ticks
+#fig.autofmt_xdate()
+
+# Datumsbeschrifung drehen
+plt.xticks(rotation=45)
+
 plt.plot(x, y)
 
 plt.show()
